e190c7d76cd250ba4e8999374a33a80fda301aa4
viraj-gavade <vrajgavade17@gmail.com>
2026-05-02 20:37:23 +0530

    yaml added

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
 from utils.response import CustomResponse
-from DataBase.connect import engine
-from Routes.auth_routes import AuthRouter
-from Routes.user_routes import UserRouter
-from Routes.blog_routes import BlogRouter
-from Routes.like_routes import LikeRouter
-from Routes.comment_routes import CommentsRouter
+# from DataBase.connect import engine
+# from Routes.auth_routes import AuthRouter
+# from Routes.user_routes import UserRouter
+# from Routes.blog_routes import BlogRouter
+# from Routes.like_routes import LikeRouter
+# from Routes.comment_routes import CommentsRouter
 
-import Models.sql_models
+# import Models.sql_models
 
-Models.sql_models.Base.metadata.create_all(engine)
+# Models.sql_models.Base.metadata.create_all(engine)
 
 
 app = FastAPI()
 
-app.include_router(AuthRouter,prefix='/auth',tags=['auth'])
-app.include_router(UserRouter,prefix='/user',tags=['user'])
-app.include_router(BlogRouter,prefix='/blog',tags=['blog'])
-app.include_router(LikeRouter,prefix='/like',tags=['Like'])
-app.include_router(CommentsRouter,prefix='/comment',tags=['comment'])
+# app.include_router(AuthRouter,prefix='/auth',tags=['auth'])
+# app.include_router(UserRouter,prefix='/user',tags=['user'])
+# app.include_router(BlogRouter,prefix='/blog',tags=['blog'])
+# app.include_router(LikeRouter,prefix='/like',tags=['Like'])
+# app.include_router(CommentsRouter,prefix='/comment',tags=['comment'])
 
 
 
@@ -27,3 +27,5 @@ def get_home():
     return CustomResponse.success(
         message='Blogs Api Running Successfully!'
     )
+
+
